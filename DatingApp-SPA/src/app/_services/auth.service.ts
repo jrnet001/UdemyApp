f28cdc512b5fc8fee25701817de34bff550217d0
John Romano <jromano@localhost.localdomain>
2019-01-09 12:19:18 -0500

    End of section 4

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+constructor(private http: HttpClient) { }
+
+baseURL = 'http://localhost:5000/api/auth/';
+
+  login(model: any) {
+      return this.http.post(this.baseURL + 'login', model).pipe(
+        map( (response: any) => {
+          const user = response;
+          if (user) {
+            localStorage.setItem('token', user.token);
+          }
+        })
+      );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseURL + 'register', model);
+  }
+}
